2dfa37691bc5136f9c405982286289721a541e4b
anto685 <garbonretoy@gmail.com>
2026-08-30 10:27:12 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ const BOT_START_TIME = Math.floor(Date.now() / 1000);
 const ADMIN_ID = 6947353037;
 
 // ТВОЯ ГИФКА РУЛЕТКИ
-const ROULETTE_GIF_ID = 'https://media.giphy.com/media/l41YtZOb9EUwkiSiA/giphy.gif';
+const ROULETTE_GIF_ID = 'const ROULETTE_GIF_ID = 'https://i.gifer.com/Origin.gif';
+';
 
 
 
